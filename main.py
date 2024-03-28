5ba52a0baeb0eba1157dbd26f9cafb4908e354fe
STaninnat <taninnat.s@gmailcom>
2024-03-29 00:18:48 +0700

    update

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+
 def main():
-    word = []
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    #print(file_contents)
-    word = file_contents.split()
-    print(len(word))
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    #num_words = get_num_words(text)
+    #print(f"{num_words} words found in the document")
+    
+    
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_txt_set(text):
+    
+
 main()
+
+
+#delete duplicates name
+    #del_name = list(dict.fromkeys(enemy_names))
