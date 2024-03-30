0a9d1dc19b401fc5e347acb5ce5ef438ae4653ae
STaninnat <taninnat.s@gmailcom>
2024-03-31 00:24:46 +0700

    update

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    #print(f"{num_words} words found in the document")
-    #####################################################
     unique = get_txt_unique(text)
     count_unique = get_count_unique(text, unique)
-    print(count_unique)
-       
+    sorted_unique = sorted(count_unique.items(), key=get_sort_on, reverse=True)
+    #unique_sentense = get_sentense(sorted_unique)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    for nam in sorted_unique:
+        print (f"The {nam[0]} character was found {nam[1]} times")
+    print("--- End report ---")
+    
+      
 def get_num_words(text):
     words = text.split()
-    return words
+    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
@@ -34,5 +39,8 @@ def get_count_unique(text, unique):
         count = lower_txt.count(char)
         output[char] = count
     return output
+
+def get_sort_on(item):
+    return item[1]
 
 main()
